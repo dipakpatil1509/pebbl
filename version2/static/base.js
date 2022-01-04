47657e91a664cd9ec047de3cfb60a25ebe4c9cb4
dipak-1509 <dipak@chillitray.com>
2022-01-04 11:52:13 +0530

    Updated Desktop Version

--- version2/static/base.js
+++ version2/static/base.js
@@ -1,14 +1,24 @@
 $(function(){
     $("#header").load("header.html"); 
-    $("#footer").load("footer.html"); 
+    $("#footer").load("footer.html");
+    
+    const mobileWidth = 750;
+
     $('.tabs-button').click((e)=>{
-        $('.tabs-button').removeClass('active');
-        $('.tabs-button').css('background-color', `var(--white)`)
+        if(window.innerWidth <= mobileWidth){
+            $('.tabs-mobile .tabs-button').removeClass('active');
+            $('.tabs-mobile .tabs-button').css('background-color', `var(--white)`);
+            $(e.target).addClass('active');
+            $(e.target).css('background-color', `var(--${$(e.target).data('button')})`);
+        }else{
+            $('.tabs-button').removeClass('active');
+            $('.tabs-desktop.active').removeClass('active');
+            $(e.target.hash).find('.tabs-desktop').addClass('active');
+            $(e.target.hash).find(`a[href="${e.target.hash}"]`).addClass('active');
+        }
         $('.tab-section').removeClass('active');
-        $(e.target).addClass('active');
         $(e.target.hash).addClass('active');
-        $('.tabs-div').css('background-color', `var(--${$(e.target).data('color')})`)
-        $(e.target).css('background-color', `var(--${$(e.target).data('color')})`)
+        $('.tabs-div').css('background-color', `var(--${$(e.target).data('color')})`);
     })
 
     var location = window.location.toString();
@@ -19,8 +29,13 @@ $(function(){
 
     if(id && validId.indexOf(id.replace('#', '')) > 0){
         $('.tabs-button').removeClass('active');
-        $('.tabs-button').css('background-color', `var(--white)`)
-        $('.tabs-button[href="'+ id +'"]').addClass('active')
+        
+        if(window.innerWidth <= mobileWidth){
+            $('.tabs-mobile .tabs-button').css('background-color', `var(--white)`);
+            $('.tabs-mobile .tabs-button[href="'+ id +'"]').addClass('active');
+        }else{
+            $('.tabs-desktop .tabs-button[href="'+ id +'"]').addClass('active');
+        }
     }
 
     function isEmail(email) {
@@ -43,8 +58,13 @@ $(function(){
                 e.stopImmediatePropagation();
                 return
             }
-        })
-        $('.tabs-button.active').trigger('click')
+        });
+        
+        if(window.innerWidth <= mobileWidth){
+            $('.tabs-mobile .tabs-button.active').trigger('click');
+        }else{
+            $('.tabs-desktop.active .tabs-button.active').trigger('click');
+        }
         $('.modal-trigger').click((e)=>{
             e.preventDefault();
             $('#formModal').css('display', 'flex')
